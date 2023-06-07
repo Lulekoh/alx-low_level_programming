b566db1421fd0b4bd0d1f60bf1ff214f80db33ba
Lulekoh <lulekoleoquintus12@gmail.com>
2023-06-07 06:51:27 -0700

    0x08. C - Recursion, all scripts and functions updated, description for each file available in the readme file at root of the repository

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recusrsion(char *s);
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, and 0 if it not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (chwck_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: interator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrime, 0 if not.
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
