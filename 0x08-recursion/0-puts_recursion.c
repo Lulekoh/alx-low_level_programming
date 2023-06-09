e47c2306201b8f1131608dce30c1cc8ad2f3b3e1
Lulekoh <lulekoleoquintus12@gmail.com>
2023-06-09 12:12:11 -0700

    0-puts_recursion.c, script or function to print a string updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
-		return (0);
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
-
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	_putchar('\n');
+	return (0);
 }
