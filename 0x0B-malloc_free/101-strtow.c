78fd067f302b86e6c5a1f8ee2c89ed8622c4e018
Lulekoh <lulekoleoquintus12@gmail.com>
2023-06-14 14:21:28 -0700

    0x0B-malloc_free, advanced file functions updated

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_word - helper function to count the number of words in a string.
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_number(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0, s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL if (Error)
+ */
+char **strtow(char *str)
+{
+
